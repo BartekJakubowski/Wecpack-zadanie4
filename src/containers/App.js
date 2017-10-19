088bde96d0ec688a38f7ce7a2fb5042662698745
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-10-20 00:07:15 +0200

    Now working

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,36 +3,34 @@ import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title.js'
 
-class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        };
-    }
-    
-    addToDo(val) {
-        const todo = {
-            text: val,
-            id: uuid.v4()
-        };
-        const data = [...this.state.data, todo]; //push todo na koncu tablicy
-        this.setState({data}); //({data}) to samo co ({data: data})
-    }
-    
-    removeTodo(id) {
-        const remainder = this.state.data.filter(todo => todo.id !== id);
-        this.setState({data: remainder});
-    }
-    
-    render() {
-        return (
-            <div className={style.TodoApp}>
-                Komponenty appki
-                <Title title='To Do List' number={this.state.data.length}/>
-            </div>
-        );
-    }
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+	addTodo(val){
+		const todo = {
+			text: val,
+			id: uuid.v4(),
+		};
+		const data = [...this.state.data, todo];
+		this.setState({data});
+	}
+	removeTodo(id) {
+		const remainder = this.state.data.filter(todo => todo.id !== id);
+		this.setState({data: remainder});
+	}
+
+	render() {
+		return ( // Tu pojawią się nowe komponenty np, Title
+			<div className={style.TodoApp}>
+				Tutaj pojawią się komponenty naszej aplikacji.
+				<Title title='Aplikacja ToDo' number={this.state.data.length}/> 
+			</div>
+		);
+	}
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,27 @@
 module.exports = {
-	entry: './src/index.js',
-		output: {
-			path: __dirname + '/build',
-			filename: 'app.bundle.js'
-		},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: "babel-loader",
-				options: {
-					presets: ['es2015']
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: 'style-loader'},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
-						}
-					}
-				]
-			}
-		]
-	}
+    entry: './src/index.js',
+        output: {
+            path: __dirname + '/build',
+            filename: 'app.bundle.js'
+        },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }       
+        ]
+    }
 };
